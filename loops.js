a3dc116ea01f684b4358baba0870e36808747888
Godwin254 <godwinbee15@gmail.com>
2020-03-19 15:26:50 -0400

    more examples on loops

---
+++ loops.js
@@ -0,0 +1,17 @@
+//1.for loop
+//2.while loop
+//3.do-while loop
+//4.for-in loop
+//5.for-each loop
+
+/*-----for loop-----*/
+for (initilization; CSSConditionRule; increment){
+     //block of code to executed
+}
+
+
+//example
+
+for ( i = 0; i < 10; i++){
+     console.log(`count is ${i} <br/>`);
+}
