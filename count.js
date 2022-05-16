01a64540519a347f7f63bda66b1836cba0a16208
Godwin254 <godwinbee15@gmail.com>
2022-05-16 08:49:04 +0300

    modify solutions

--- count.js
+++ count.js
@@ -40,3 +40,41 @@ function golfScore(par, strokes) {
 }
 
 console.log(golfScore(5, 4));
+
+
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+
+  if (value === ''){
+    delete records[id][prop];
+  }else if (prop === 'tracks'){
+    records[id][prop] = records[id][prop] || [];
+    records[id][prop].push(...value);
+  }else{
+    records[id][prop] = value;
+  }
+  return records;
+}
+
+console.log(updateRecords(recordCollection, 5439, 'tracks', ["qwen", "takila"]));
