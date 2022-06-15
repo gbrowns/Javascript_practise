6ab1a50faba3270e1446df62da6537407d97dbf7
Godwin254 <godwinbee15@gmail.com>
2022-06-15 21:38:30 +0300

    lesson --15-06-22

---
+++ lessons.js
@@ -0,0 +1,81 @@
+// lesson examples for j-chep
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+let num = null;   
+//console.log(typeof(num));
+
+
+if (typeof(num) === "undefined"){
+    console.log("please assign some value to the variable");
+}else{
+   // console.log("you have successfully assigned a value");
+}
+
+let id1 = Symbol(1);
+let id2 = Symbol(1);
+
+const person = {
+    name: "John",
+    age: 25
+}
+
+console.log(person.instanceOf(name))
+/*if (id1 === id2 ){
+    console.log("This ids are similar");
+    console.log("please have different ids");
+    
+    
+}else{
+    console.log("Your ids are unique(are not same)");
+    
+}*/
+let age = 67
+let isAdult = (age > 18) ? "Is and Adult" : "Not Adult";
+console.log(isAdult);
+/*(id1 === id2 ) ?  console.log("please have different ids") 
+               : console.log("Your ids are unique(are not same)");*/
+
+/*let name = "John";
+let age = 20;
+
+console.log(`Good morning ${name}, your age is ${age} years old`);
+console.log("Good morning" + " "+ name + ","+" your age is" + " "+ age + " "+ " years old")*/
+
+
+
+/*let a = 4
+let b = 6
+
+//comparison
+if (a > b){
+    //what happens
+}
+
+if (age != 18){
+    //
+}
+
+
+if (b >= a){
+    //do something
+}
+
+for(let i = 0; i <= 6; i++){
+    //do somthing
+    console.log(i)
+}
+*/
+
+
+// //js - dynamically typed langyae
+// let num = "tjhgkh"; //string
+// let num2 = 2 ; //number
+
+// //java | C | C++ - statically typed languages
+// num = "hgkyhk" //error
+// String num2 " String"
+
+// //python - dynamically typed
+// num = "gfutf" //sting
+// num2 = 12 //number
